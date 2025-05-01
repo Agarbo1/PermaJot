333bd1d4c599daa1984df154a4cda56364402390
Alex Garbarino <alexiothebrave@gmail.com>
2025-04-30 20:01:55 -0500

    models/migrations added, need to tweak a few things fot tags, tasks, and notetags

---
+++ backend/db/migrations/20250501001224-create-note.js
@@ -0,0 +1,40 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Notes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      notebookId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Notebooks',
+          key: 'id'
+        },
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      content: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Notes');
+  }
+};
