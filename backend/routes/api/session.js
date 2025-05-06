c564d7438a1804a094d42cd89ecdec35496ce565
Alex Garbarino <alexiothebrave@gmail.com>
2025-05-05 20:07:20 -0500

    added login validation middleware

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -2,18 +2,27 @@
 const express = require('express');
 const { Op } = require('sequelize');
 const bcrypt = require('bcryptjs');
-
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 const { setTokenCookie, restoreUser } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
 
 const router = express.Router();
 
-// Log in
-// backend/routes/api/session.js
-// ...
+// Validation middleware
+const validateLogin = [
+  check('credential')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Please provide a valid email or username.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a password.'),
+  handleValidationErrors,
+];
 
 // Log in
-router.post('/', async (req, res, next) => {
+router.post('/', validateLogin, async (req, res, next) => {
   const { credential, password } = req.body;
 
   const user = await User.unscoped().findOne({
