0a6d5c216d05d2e81425e41dfa19ee87dc1ab40a
Alex Garbarino <alexiothebrave@gmail.com>
2025-05-19 20:56:06 -0500

    verified indices were formatted properly

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,10 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
 import configureStore from "./store";
 import { restoreCSRF, csrfFetch } from "./store/csrf";
 import * as sessionActions from './store/session';
-import { Modal, ModalProvider } from "./context/Modal";
+import { ModalProvider } from "./context/Modal";
 import { BrowserRouter } from 'react-router-dom';
 
 const store = configureStore();
