da751569ca3875d621d149b2fddfc0d8b03b18ba
Alex Garbarino <alexiothebrave@gmail.com>
2025-05-19 20:46:27 -0500

    m

--- backend/app.js
+++ backend/app.js
@@ -79,7 +79,7 @@ app.use(express.static(path.resolve(__dirname, '../frontend/dist')));
 // Serve React frontend for all other routes
 app.use((req, res, next) => {
   if (req.method === 'GET' && !req.path.startsWith('/api')) {
-    res.sendFile(path.resolve(__dirname, '../frontend/dist/index.html'));
+    res.sendFile(path.resolve(__dirname, '../frontend/index.html'));
   } else {
     next();
   }
